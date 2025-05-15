55c7b95dacf6786b2bd95ac6190aeec89a97f577
Thiago Santos <137359517+Thwo22@users.noreply.github.com>
2025-05-15 18:47:50 -0300

    1.3

--- script.js
+++ script.js
@@ -3,6 +3,34 @@ const campoResposta1 = document.getElementById("resposta1");
 const campoResposta2 = document.getElementById("resposta2");
 const campoResposta3 = document.getElementById("resposta3");
 const campoResposta4 = document.getElementById("resposta4");
+const botaoResetar= document.getElementById("reset");
+
+const somRespostaCerta = document.getElementById("somCorreto");
+const somRespostaErrada = document.getElementById ("somErrado");
+const acertouMais = document.getElementById("final-feliz");
+const acertouMenos = document.getElementById("final-triste");
+
+
+const containerDeRepostas = document.getElementById("box");
+const imagemHappyEnd = document.createElement("img");
+imagemHappyEnd.src = "src/imagens/finalizado.jpg";
+imagemHappyEnd.alt = "parabens-foto";
+imagemHappyEnd.style.width = "200px";
+imagemHappyEnd.style.height = "200px";
+imagemHappyEnd.style.position = "relative";
+imagemHappyEnd.style.left = "60%";
+imagemHappyEnd.style.top = "5%";
+
+const imagemBadEnd = document.createElement("img");
+imagemBadEnd.src = "src/imagens/finalizado2.jpg";
+imagemBadEnd.alt = "tente-novamente-foto";
+imagemBadEnd.style.width = "200px";
+imagemBadEnd.style.height = "200px";
+imagemBadEnd.style.position = "relative";
+imagemBadEnd.style.left = "60%";
+imagemBadEnd.style.top = "5%";
+
+botaoResetar.style.display = "none"
 
 const perguntas = [
 {
@@ -63,8 +91,10 @@ function verificarResposta(respostaEscolhida, botaoClicado) {
     if (respostaEscolhida === questao.correta) {
         botaoClicado.style.backgroundColor = "#4CAF50";
         botaoClicado.style.color = "white";
+        somRespostaCerta.play();
         acertos++;
     } else {
+        somRespostaErrada.play();
         botaoClicado.style.backgroundColor = "#FF0000";
         botaoClicado.style.color = "white";
         botaoCorreto.style.backgroundColor = "#4CAF50";
@@ -81,9 +111,22 @@ function verificarResposta(respostaEscolhida, botaoClicado) {
         } else {
             campoDeQuestao.innerText = `quiz Finalizado! acertos: ${acertos}/${perguntas.length}`;
             [campoResposta1, campoResposta2, campoResposta3, campoResposta4].forEach(btn => btn.style.display = "none");
+            botaoResetar.style.display = "block";
+            
+            if (acertos > 3) {
+                acertouMais.play()
+                containerDeRepostas.appendChild(imagemHappyEnd);
+            } else {
+                acertouMenos.play()
+                containerDeRepostas.appendChild(imagemBadEnd);
+            }
         }
     }, 1000);
 }
+
+    botaoResetar.addEventListener("click", () => {
+            window.location.reload();
+    });
 
 mostrarQuestao(0);
 
